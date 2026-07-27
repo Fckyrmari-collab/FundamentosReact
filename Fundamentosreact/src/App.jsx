4fa1816ae11b2767e32e9dd86359485a52bffbcf
Fckyrmari-collab <https://github.com/Fckyrmari-collab/FundamentosReact.git>
2026-07-27 12:14:09 -0500

    Hoja de vidaaa

--- Fundamentosreact/src/App.jsx
+++ Fundamentosreact/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import foto from './assets/imagen.jpg'
 import './App.css'
 
 function App() {
@@ -9,112 +7,31 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1><Bienvenidos></Bienvenidos></h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+     <div>
+      <h1>ANALISIS Y DESARROLLO DE SOFTWARE</h1>
+      <h2>TECNOLOGO</h2>
+      <p>HOJA DE VIDA APRENDIZ</p>
+      <img src={foto} alt="Foto de inicio" width="250"/>
+      <h2>MARIA PAULA SOLANO COMBARIZA </h2>
+      <h2>19</h2>
+      <h2>BOGOTÁ DC</h2>
+      <h2>dinosolispau679@gmail.com</h2>
+      <h2>ADSO</h2>
+     </div>
+     <div>
+      <h1>CURSOS REALIZADOS</h1>
+      <h2>Tenico en Sistemas</h2>
+      <h2>Desarollo de hadware</h2>
+     </div>
+     <div>
+      <h1>OBJETIVO PROFESIONAL</h1>
+      <h2> Deseo adquirir experiencia en el desarrollo de interfaces interactivas y mejorar mis habilidades como desarrollador de software.</h2>
+      <h2>Por que deseas aprender react? - ¿En qué tipo de proyectos le gustaría utilizarlo? </h2>
+      <h2>
+        Deseo aprender React para desarrollar aplicaciones web modernas y mejorar mis habilidades como desarrollador. Me gustaría utilizar esta tecnología en proyectos como sistemas de gestión, plataformas de datos y otras aplicaciones que ofrezcan una buena experiencia al usuario.
+      </h2>
+     </div>
+     <button>Conoce más sobre mí</button>
     </>
   )
 }
